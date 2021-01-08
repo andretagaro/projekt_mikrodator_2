933282653efcd469bc21e91dc1aa3a169f2ded2b
snowman1332 <36790413+snowman1332@users.noreply.github.com>
2021-01-08 12:23:32 +0100

    Update and rename nod_test.js to mqtt_communication.js

--- oliviers_kod/mqtt_communication.js
+++ oliviers_kod/mqtt_communication.js
@@ -1,15 +1,14 @@
 
-
-const serial_options = {baudRate: 9600}; // Set baud-rate
-const serial_port = "COM13";  // Declare port name
+const serial_options = {baudRate: 9600};
+const serial_port = "COM13";
 
 const serialport = require('serialport');
 let myPort = new serialport(serial_port, serial_options);
 
-// Set a parser to use for the data on the serial port
-let Readline = serialport.parsers.Readline; // parser to read ASCII lines
-let parser = new Readline(); // make parser
-myPort.pipe(parser); // pipe the serial stream to the parser
+
+let Readline = serialport.parsers.Readline;
+let parser = new Readline();
+myPort.pipe(parser);
 
 //Map callback for incomming data events on parser
 myPort.on('open', showPortOpen);
@@ -18,25 +17,26 @@ myPort.on('close', showPortClose);
 myPort.on('error', showError);
 
 
-//Callbacks for events on serial port
 function showPortOpen() {
   console.log('port open. Data rate: ' + myPort.baudRate);
 }
 
-var read_data1 = 0;
-var read_data2 = 0;
+//Global variable to store incoming data
+var read_data1 = "off";
+var read_data2 = "off";
 
 //Data från serialport (uart received)
-// First bite determine where to store data
+
 function readSerialData(data) {
   console.log("Program ended");
   console.log('Data received: ', data)
 
+  // First bit determine where to store data
   if (data[0] == "A") {
-    read_data2 = data;
+    read_data2 = data;  // This will be sent to one topic
   }
   if (data[0] == "B") {
-    read_data1 = data;
+    read_data1 = data;  //This will be sent to another topic
   }
 }
 
@@ -55,7 +55,7 @@ function showError(error) {
 
 
 //Write first data to serialport
-
+// text is globalvariable declare to receive data from mqtt
 let timerVer = setInterval(sendSerialData, 900);
 function sendSerialData() {
 
@@ -66,8 +66,7 @@ function sendSerialData() {
 
 
 //Second data to serialport
-
-//var text1 = 'B2001';
+//// text2 is globalvariable declare to receive second data from mqtt
 let timerVer1 = setInterval(sendSerialData2, 910);
 function sendSerialData2() {
   console.log('send_serial2: ' + text2)
@@ -75,6 +74,14 @@ function sendSerialData2() {
 }
 
 //************MQTT SETUP******************
+
+// Default values if no data comes from mqtt
+var text = "C";
+var text2 =  "D";
+
+//Global variable to store counting values
+var count_var = 1;
+var count_var2 = 1;
 
 // Initiate mqtt setup requirement
 let mqtt = require('mqtt');
@@ -88,27 +95,27 @@ connect_options = {
 };
 
 
-//UNDO THIS************************************************
 let mqtt_client = mqtt.connect(mqtt_server, connect_options);
 mqtt_client.on("connect", function() {
     console.log("connection: " + mqtt_client.connected)
 
 })
 
-//*********************test*******************************
-var count_var = 1;
-var count_var2 = 1;
+  //*****************A topic to check if a car goes offline*****************
+  const offline_check = function () {
+       mqtt_client.publish('grupp4/range/check', count_var + '\n');
+   }
+   const timer_check = setInterval(offline_check, 1000);
 
-const test_publish = function() {
-  mqtt_client.publish('grupp4/range/test', count_var + '\n');
-}
-const timer_test = setInterval(test_publish, 1000);
+
 
   //******************Publish to car A***********************
  const publish_this_1 = function () {
-      mqtt_client.publish('grupp4/direction/remote_2', read_data2 + '\n');
+      mqtt_client.publish('grupp4/direction/remote_2', read_data2 );
   }
   const timer_count1 = setInterval(publish_this_1, 250);
+
+
 
   //****************Publish to car B************************
     const publish_this = function () {
@@ -119,29 +126,24 @@ const timer_test = setInterval(test_publish, 1000);
 
   //Subscribe to multiple topics by format topic/sub_topic/#
   let topic_in = "grupp4/range/#";
-
   mqtt_client.subscribe(topic_in, {qos:1});
 
-  // Default values if no data comes from mqtt
-  var text = "C";
-  var text2 =  "D";
-
-  //Set up a callback event for incoming messages on mqtt
   //One connection to multiple topics
   mqtt_client.on("message", function(topic_in, message, packet) {
 
-      if (topic_in == 'grupp4/range/test') {
-        count_var++;
-        count_var2++;
-      }
+    //grupp4/range/check is always online when program executes
+    if (topic_in == 'grupp4/range/check') {
+      count_var++;
+      count_var2++;
 
-    // If no new data is received after 10s count_var > 20
+    }
+
+    // If no new data is received after before count_var > 20
     if ((topic_in == 'grupp4/range/car_2') || (count_var > 20)) {
       console.log("topic is: " + topic_in);
       console.log("Message is: " + message);
 
-      // If topic is not online send C else send message
-      // Increment of count_var to a value over 20 means the topic is offline
+      //Topic is offline if the statement evaluate true else send received message
       if ((message == "ANaN") || (count_var > 20)) {
         text = "C";
       }
@@ -156,8 +158,7 @@ const timer_test = setInterval(test_publish, 1000);
       console.log("topic is: " + topic_in);
       console.log("Message is: " + message);
 
-      // If topic is not online send D else send message2
-      // Increment of count_var to a value over 20 means the topic is offline
+      //Topic is offline if the statement evaluate true else send received message
       if ((message == "ANaN") || (count_var2 > 20)) {
         text2 = "D";
       }
@@ -168,6 +169,5 @@ const timer_test = setInterval(test_publish, 1000);
     }
   })
 
-//UNDO THIS************************************************
 
 //
